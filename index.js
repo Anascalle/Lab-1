e856e87bdd4f1ea07da83a40850f257c27a6fe71
Ana Sofia Calle Cordoba <1005894876@u.icesi.edu.co>
2023-08-11 19:00:20 -0500

    Laboratorio 1, birthbirthday-calculator,round-to

---
+++ index.js
@@ -0,0 +1,5 @@
+const calculator= require('birthday-calculator');
+const date = new Date('2003,02,09');
+const result = calculator.getDayWhenBorn(date);
+console.log(result);
+
---
+++ money.js
@@ -0,0 +1,8 @@
+
+import {roundTo, roundToUp, roundToDown} from 'round-to';
+
+roundTo(5666.78, -2);
+roundToDown(1.922, 2);
+roundToUp(3.876, 2);
+
+console.log(roundTo(5666.78, -2))
